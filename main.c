9ed52c03642625f08c5fdce6864e4abfea240af6
Luke7720 <81709312+0x4c37373230@users.noreply.github.com>
2022-01-04 11:58:47 -0300

    Biome finder base

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "./cubiomes/generator.h"
+#include "cubiomes/finders.h"
+
+int main()
+{
+    initBiomes();
+
+    LayerStack layers = setupGeneratorMCBE17();
+    int64_t seed = -395202839;
+    Pos pos = { -260, -76 };
+
+    applySeed(&layers, seed);
+
+    int biomeId = getBiomeAtPos(layers, pos);
+
+    printf("%d", biomeId);
+
+    return 0;
+}
