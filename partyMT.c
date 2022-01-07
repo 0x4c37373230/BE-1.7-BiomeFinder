3dc8dc0dc440391e2c426047b0b7b4ff76f44cfe
Joshbuild10 <88260146+Joshbuild10@users.noreply.github.com>
2022-01-07 17:09:08 +0100

    Cleaned up code

--- partyMT.c
+++ partyMT.c
@@ -17,7 +17,7 @@ uint32_t* partyMT(uint32_t seed, int n)
     uint32_t* result = malloc(n*sizeof(*head_arr));
     head_arr[0] = seed;
 
-    for (int i = 1; i < n + 1; i++)
+    for (int i = 1; i <= n; i++)
         INIT(head_arr[i], head_arr[i - 1], i);
 
     uint32_t temp = head_arr[n];
@@ -27,7 +27,7 @@ uint32_t* partyMT(uint32_t seed, int n)
 
     last_arr[0] = temp;
 
-    for (int i = 1; i < n + 1; ++i)
+    for (int i = 1; i <= n; ++i)
         INIT(last_arr[i], last_arr[i - 1], i + 397);
 
     for (int i = 0; i < n; i++)
@@ -35,17 +35,17 @@ uint32_t* partyMT(uint32_t seed, int n)
         temp = (head_arr[i] & 0x80000000) + (head_arr[i + 1] & 0x7fffffffu);
         head_arr[i] = (temp >> 1u) ^ last_arr[i];
 
-        if (temp % 2 != 0)
-            head_arr[i] = head_arr[i] ^ 0x9908b0df;
+        if (temp % 2)
+            head_arr[i] ^= 0x9908b0df;
     }
 
     for (int i = 0; i < n; ++i)
     {
         uint32_t y = head_arr[i];
-        y = y ^ y >> 11u;
-        y = y ^ y << 7u & 2636928640u;
-        y = y ^ y << 15u & 4022730752u;
-        y = y ^ y >> 18u;
+        y ^= y >> 11u;
+        y ^= y << 7u & 2636928640u;
+        y ^= y << 15u & 4022730752u;
+        y ^= y >> 18u;
         result[i] = y;
     }
 
